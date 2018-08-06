bc5c9d708db008a7a65ee37c5e605d8cebe027cf
marviorocha <marviorocha@marviorocha.com>
2018-08-05 22:36:11 -0300

    Fix DB for Deploy

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'stationpro');
+define('DB_NAME', 'userNXP');
 
 /** MySQL database username */
 define('DB_USER', 'stationpro');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'luacho07');
+define('DB_PASSWORD', 'EgjMRHCGbB26SMr4');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
