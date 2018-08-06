7a2300b099ea1a6ec630dfc4921301b6a6bfcb3b
marviorocha <marviorocha@marviorocha.com>
2018-08-05 23:09:38 -0300

    add mariadb

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'userNXP');
+define('DB_NAME', 'station-wp');
 
 /** MySQL database username */
 define('DB_USER', 'stationpro');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'EgjMRHCGbB26SMr4');
+define('DB_PASSWORD', 'luacho07');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mariadb');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
